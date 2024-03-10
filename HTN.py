51e349e5614129a41d6474734488c1bb77392a90
lkaldes <lkaldes@ucsc.edu>
2024-03-10 16:42:56 -0700

    Cleaned up comments

--- HTN.py
+++ HTN.py
@@ -14,13 +14,13 @@ def create_dicts(data):
 
     return wordlist, attrlist
 
+# guess a random word/item from current list of possible choices
 def rng_guess(words):
     guess = random.choice(words)
     return guess
     
 
 def eliminate(words, attr, goal, guess, time):
-    #print(guess)
     # if guess word is same as goal word, found it
     if goal == guess:
         return guess, time
@@ -50,19 +50,16 @@ def eliminate(words, attr, goal, guess, time):
     # run through all words/items in list and remove any that don't match the new criteria
     for x in attr:
         for i in range(len(compared) - 1):
-            #print(words[attr.index(x)])
             if isinstance(compared[i], str):
                 # if goal > guess, remove any words/items that are smaller or same
                 if compared[i] == ">":
                     if list(x.values())[i] <= list(guess_attr.values())[i]:
-                        #print("removing", words[attr.index(x)], "for being too small", list(x.keys())[i])
                         new_words.remove(words[attr.index(x)])
                         new_attr.remove(x)
                         break
                 # if goal < guess, remove any words/items that are bigger or same
                 else:
                     if list(x.values())[i] >= list(guess_attr.values())[i]:
-                        #print("removing", words[attr.index(x)], "for being too big", list(x.keys())[i])
                         new_words.remove(words[attr.index(x)])
                         new_attr.remove(x)
                         break
@@ -70,13 +67,10 @@ def eliminate(words, attr, goal, guess, time):
             else:
                 if compared[i] == True:
                     if list(x.values())[i] != list(guess_attr.values())[i]:
-                        #print(list(x.values())[i], list(guess_attr.values())[i])
-                        #print("removing", words[attr.index(x)], "for not being the same", list(x.keys())[i])
                         new_words.remove(words[attr.index(x)])
                         new_attr.remove(x)
                         break
-    #print(new_words)
-    #repeat process again until found last one [Be sure to swap out what goes into the "guess" slot]
+    # repeat process again until found last one [Be sure to swap out what goes into the "guess" slot]
     return eliminate(new_words, new_attr, goal, rng_guess(new_words), time)
 
 # main function, read json file into list of words/items. Then plays the game
@@ -92,7 +86,7 @@ if __name__ == '__main__':
     print("Goal is", goalword)
     time = 0
 
-    #guess = half_eliminate(words, attr)
+    # guess = half_eliminate(words, attr) [EXAMPLE]
     guess = rng_guess(words)
     final, time = eliminate(words, attr, goalword, guess, time)
     print("Final Guess is:", final)
